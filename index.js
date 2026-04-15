e9be7c92f562e66b2e20009bb8e6631009b0e20e
kunal3003 <tdZAmc3-Jez1qh-y1Urq>
2026-04-14 21:42:34 -0400

    feature1 change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dbConnect from "./config/dbConnect.js"
 
 const StartServer = async () => {
     app.listen(PORT, () => {
-        console.log(`Server Listening on port ${PORT}`)
+        console.log(`Order API Server Listening on port ${PORT}`)
     }).on('error', (err) => {
         debug(`Error: ${err}`)
         process.exit(1)
