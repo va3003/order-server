4e3a90104e0402b5caf8e202e63380735644435e
kunal3003 <tdZAmc3-Jez1qh-y1Urq>
2026-04-14 22:46:31 -0400

    committing feature 4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dbConnect from "./config/dbConnect.js"
 
 const StartServer = async () => {
     app.listen(PORT, () => {
-        console.log(`Feature3 Server Listening on port ${PORT}`)
+        console.log(`Feature 4 Server Listening on port ${PORT}`)
     }).on('error', (err) => {
         debug(`Error: ${err}`)
         process.exit(1)
